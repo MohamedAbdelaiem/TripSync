c417221d8f01a6e1e4f620ccb3702496429c5746
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2024-11-30 01:48:40 +0200

    Finish Rewards Back_end

--- Server/Routes/RewardsRouter.js
+++ Server/Routes/RewardsRouter.js
@@ -6,6 +6,10 @@ const rewardsRouter = express.Router();
 rewardsRouter.route('/getAllRewards').get(AuthController.protect,RewardsController.getAllRewards);
 rewardsRouter.route('/getRewardById/:reward_id').get(AuthController.protect,RewardsController.getRewardById);
 rewardsRouter.route('/addReward').post(AuthController.protect,AuthController.restrictTo("admin"),RewardsController.addReward);
+rewardsRouter.route('/deleteReward/:reward_id').delete(AuthController.protect,AuthController.restrictTo("admin"),RewardsController.deleteReward);
+rewardsRouter.route('/updateReward/:reward_id').patch(AuthController.protect,AuthController.restrictTo("admin"),RewardsController.updateReward);
+rewardsRouter.route('/RedeemReward/:reward_id').post(AuthController.protect,AuthController.restrictTo("traveller"),RewardsController.RedeemReward);
+
 
 
 

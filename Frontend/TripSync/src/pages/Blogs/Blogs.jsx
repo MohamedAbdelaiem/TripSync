548ce252ca142fd11e39a5c280eb385884822b91
Esraa-Hassan0 <esraa.elfatairy04@eng-st.cu.edu.eg>
2024-12-18 22:09:10 +0200

    fix error

--- Frontend/TripSync/src/pages/Blogs/Blogs.jsx
+++ Frontend/TripSync/src/pages/Blogs/Blogs.jsx
@@ -5,8 +5,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import BlogPost from "../../Components/Blog/Blog";
 import "./Blogs.css";
-import NavbarSignedIn from "../../Components/NavbarSignedIn/NavbarSignedIn";
-import { NavLink } from "react-router-dom";
+
 const BASE_URL = "http://localhost:3000/api/v1/blogs/AllBlogs";
 
 function Blogs() {
@@ -33,72 +32,58 @@ function Blogs() {
   }, []);
 
   return (
-    <>
-      {/* <NavbarSignedInner /> */}
-
-      <NavbarSignedIn />
-      <NavLink to={"/blogWrite"}>
-        <input
-          type="text"
-          placeholder="What's in your mind?"
-          className="newBlog"
-        />
-      </NavLink>
-  
-    
-            
-      <div className="blogs-page">
-        <div className="blogs-header">
-          <div className="header-content">
-            <h1>Community Blogs</h1>
-            <Link to="/blogWrite" className="create-blog-btn">
-              <Plus strokeWidth={2.5} />
-              <span>Create New Blog</span>
-            </Link>
-          </div>
-        </div>
-
-        <div className="blogs-container">
-          {isLoading ? (
-            <div className="loading-container">
-              <Loader2 className="spinner" size={48} />
-              <p>Loading blogs...</p>
-            </div>
-          ) : error ? (
-            <div className="error-container">
-              <p>{error}</p>
-            </div>
-          ) : (
-            <AnimatePresence>
-              {blogs.map((blog, index) => (
-                <motion.div
-                  key={blog.blog_id}
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: -20 }}
-                  transition={{
-                    duration: 0.3,
-                    delay: index * 0.1
-                  }}
-                >
-                  <BlogPost
-                    blog={{
-                      id: blog.blog_id,
-                      username: blog.profilename || blog.username,
-                      profilePic: blog.profilephoto,
-                      content: blog.content,
-                      image: blog.photo,
-                      date: new Date(blog.date).toLocaleDateString(),
-                      time: new Date(`1970-01-01T${blog.time}Z`).toLocaleTimeString()
-                    }}
-                  />
-                </motion.div>
-              ))}
-            </AnimatePresence>
-          )}
+    <div className="blogs-page">
+      <div className="blogs-header">
+        <div className="header-content">
+          <h1>Community Blogs</h1>
+          <Link to="/blogWrite" className="create-blog-btn">
+            <Plus strokeWidth={2.5} />
+            <span>Create New Blog</span>
+          </Link>
         </div>
       </div>
-    </>
+
+      <div className="blogs-container">
+        {isLoading ? (
+          <div className="loading-container">
+            <Loader2 className="spinner" size={48} />
+            <p>Loading blogs...</p>
+          </div>
+        ) : error ? (
+          <div className="error-container">
+            <p>{error}</p>
+          </div>
+        ) : (
+          <AnimatePresence>
+            {blogs.map((blog, index) => (
+              <motion.div
+                key={blog.blog_id}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ 
+                  duration: 0.3, 
+                  delay: index * 0.1 
+                }}
+              >
+                <BlogPost 
+                  blog={{
+                    id: blog.blog_id,
+                    username: blog.profilename || blog.username,
+                    profilePic: blog.profilephoto,
+                    content: blog.content,
+                    image: blog.photo,
+                    date: new Date(blog.date).toLocaleDateString(),
+                    time: new Date(`1970-01-01T${blog.time}Z`).toLocaleTimeString()
+                  }}
+                />
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        )}
+      </div>
+    </div>
   );
 }
+
 export default Blogs;

c4c74a24b07df1db878be7ff802c075682048a84
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2024-12-18 12:17:27 +0200

    reports in admin

--- Frontend/TripSync/src/Components/Admin/AllReports/AllReportsList.jsx
+++ Frontend/TripSync/src/Components/Admin/AllReports/AllReportsList.jsx
@@ -1,18 +1,57 @@
 import React from "react";
 import AdminReport from "./AdminReport";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "./AllReportsList.css";
 
 function AllReportsList({ all_reports, userId, rerender }) {
-  const deleteReport = (report_id) => {
-    console.log(`delete report with id: ${report_id}`);
+  const navigate = useNavigate();
+  const deleteReport = async(traveller_id,travelAgency_id) => {
+    try{
+      const token = localStorage.getItem("token");
+      await axios.delete(`http://localhost:3000/api/v1/reports/deleteReportForAdmin`, {
+        data: {
+          traveller_id: traveller_id,
+          travelAgency_id: travelAgency_id
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      });
+      rerender();
+    }
+    catch(err){
+      console.log(err);
+    }
   };
-  const blockAgency = (agency_id) => {
+  const blockAgency = async(agency_id) => {
+    await axios
+      .delete(`http://localhost:3000/api/v1/users/deleteUser`, {
+        data: {
+          user_id: id,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log("user deleted");
+        rerender();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     console.log(`block agency with id: ${agency_id}`);
   };
   const viewUserProfile = (traveller_id) => {
+    navigate(`/traveller-profile/${traveller_id}`);
     console.log(`view traveller with id: ${traveller_id}`);
   };
   const viewAgencyProfile = (agency_id) => {
+    navigate(`/travel-agency/${agency_id}`);
     console.log(`view agency with id: ${agency_id}`);
   };
   return (
